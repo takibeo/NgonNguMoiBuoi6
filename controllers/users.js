ad2041e5180de28e1aab784c2fa4c2134ca78962
takibeo <voky12460@gmail.com>
2026-03-09 14:27:42 +0700

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,32 @@
+let userModel = require('../schemas/users')
+module.exports = {
+    CreateAnUser: async function (username, password, email, role,
+        avatarUrl, fullName, status, loginCount
+    ) {
+        let newUser = new userModel({
+            username: username,
+            password: password,
+            email: email,
+            role: role,
+            avatarUrl: avatarUrl,
+            fullName: fullName,
+            status: status,
+            loginCount: loginCount
+        })
+        await newUser.save();
+        return newUser;
+    },
+    QueryByUserNameAndPassword: async function (username, password) {
+        let getUser = await userModel.findOne({ username: username });
+        if (!getUser) {
+            return false;
+        }
+        return getUser;
+    },
+    FindUserById: async function (id) {
+        return await userModel.findOne({
+            _id: id,
+            isDeleted:false
+        }).populate('role')
+    }
+}
